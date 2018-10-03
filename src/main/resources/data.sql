53fdecc787af8c4e6df43fc529be6620d53bd626
tshepisol <tshepiso.letswalo@gmail.com>
2018-10-03 12:40:43 +0200

    smart grocer: sql data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,11 +3,11 @@ insert into role (role_type) values ('CUSTOMER');
 
 insert into customer_no (customer_number) values (1);
 
-insert into partner (id, partner_type, referral) values (1,'LEVEL_1', 1);
+insert into partner (partner_type, referral) values ('LEVEL_1', 1);
 
 
-insert into vendor (id, name) values (1,'MAKRO');
+insert into vendor (name) values ('MAKRO');
 
-insert into hamper (id, name, vendor_id) values (1, 'Grocery hamper',1);
+insert into hamper (name, vendor_id) values ('Grocery hamper',1);
 
 
