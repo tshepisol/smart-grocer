d7f27d04a41014850933e7f777be231347fc04d9
tshepisol <tshepiso.letswalo@gmail.com>
2018-11-16 07:53:52 +0200

    bug fixes - JSON cyclic errors

--- src/main/java/za/co/soma/solutions/smart/grocer/Application.java
+++ src/main/java/za/co/soma/solutions/smart/grocer/Application.java
@@ -2,9 +2,11 @@ package za.co.soma.solutions.smart.grocer;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 public class
@@ -17,5 +19,14 @@ Application {
 	@Bean
 	public ObjectMapper objectMapper(){
 		return new ObjectMapper();
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		MappingJackson2HttpMessageConverter converter =
+				new MappingJackson2HttpMessageConverter(mapper);
+		return converter;
 	}
 }
